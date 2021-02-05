75cfc3f00ff1580db3f04af85c43a67fed9ffe24
KimJoongKi <thoug12@naver.com>
2021-02-06 07:45:09 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/yaddoong/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/yaddoong/datajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package yaddoong.datajpa.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +8,8 @@ import org.springframework.transaction.annotation.Transactional;
 import yaddoong.datajpa.entity.Member;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -60,6 +57,24 @@ class MemberRepositoryTest {
 
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findHelloBy() {
+        memberRepository.findTop3HelloBy();
     }
 
 }
